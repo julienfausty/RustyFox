f64823357c89cb7ea361f7a8aabf8f191f037d0d
Julien Fausty <julienfausty@gmail.com>
2023-04-16 18:57:16 +0200

    geometry: add new geometry module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! A crate providing a framework for writing finite element codes
 
+/// Module providing descriptions of geometry for the library
+pub mod geometry;
 
 /// Module providing base elements for assembly
 pub mod element;
